3d67470109fec8602b021ddf847126ef79c7ad08
Akemu Emmanuel <akemuemmy@gmail.com>
2023-10-17 08:22:53 +0100

    Fixed the bug

--- Src/main.c
+++ Src/main.c
@@ -2,6 +2,24 @@
 
 int main()
 {
-    printf("Enter a number: ");
+    int mode;
+    printf("Select 1 for standard calculator, \nSelect 2 for scientific calculator mode, \nSelect 3 for binary converter mode");
+    printf(" \n>");
+    scanf("%d", &mode);
+    
+    switch(mode){
+        case 1:
+            // call standard
+            printf("perfect");
+            break;
+        case 2:
+            // call scientific calculator
+            break;
+        case 3:
+            // call binary converter
+            break;
+        default:
+            printf("Invalid Selection");
+    }
 
 }
