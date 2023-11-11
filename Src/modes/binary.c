63230181e0f6b41cc46862b20fa9d964ca3b5fc8
CPU Governor <cpu.governor@gmail.com>
2023-11-11 12:42:59 +0100

    updated binary.c

--- Src/modes/binary.c
+++ Src/modes/binary.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
-int main/*binary to deci */ ( )
+void main()
 {
-
+long num,i,temp;
+printf("Input a number to find binary code\n");
+scanf("%ld",&num);
+while(num!=0)
+{
+i=1;
+while(i<=num/2)
+{
+i=i*2;
+}
+num=num-i;
+printf("1");
+temp=i/2;
+while(num<temp)
+{
+temp=temp/2;
+printf("0");
+}
+}
+}
 return 0;
 }
